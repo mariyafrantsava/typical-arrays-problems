623cd96fd35b981d8a8200271552402241e221d6
mariyafrantsava <mariya.frantsava7@gmail.com>
2020-10-02 22:57:26 +0300

    init: start to do ex

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
+    return Math.min(array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return Math.max(array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    return 0;
 }

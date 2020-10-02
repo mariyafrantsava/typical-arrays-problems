c901144a7934bd788574760322adf31c96223129
mariyafrantsava <mariya.frantsava7@gmail.com>
2020-10-03 00:17:57 +0300

    feat: make functions min, max and avg for arrays

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 exports.min = function min(array) {
-
-    return Math.min(array);
+    if (typeof array == "undefined" || !array.length) {
+        return 0;
+    }
+    return Math.min.apply(null, array);
 }
 
 exports.max = function max(array) {
-    return Math.max(array);
+    if (typeof array == "undefined" || !array.length) {
+        return 0;
+    }
+    return Math.max.apply(null, array);
 }
 
 exports.avg = function avg(array) {
-    return 0;
+    if (typeof array == "undefined" || !array.length) {
+        return 0;
+    }
+    let sum = 0.0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / (array.length);
 }
